d2855c394c6bcf0daacfd8160f86f94b20b45ddd
Dhruvil Galaiya <dhruvil.galaiya.btech2023@sitpune.edu.in>
2025-04-30 14:59:35 +0530

    Add files via terminal
    Added Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*
+Name: Dhruvil Galaiya
+PRN: 23070126039
+Batch: A2
+
+This is the main class that manages array-based tasks such as even-odd separation,
+neighbor distance calculation, and Array-ArrayList conversions.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Array and ArrayList Operations Menu:");
+        ArrayOperations.menu();
+    }
+}
